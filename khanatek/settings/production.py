5a9c9ffc4cbdc857e2d5acbf35f81cdbc0b4e91e
ibrobabs <babskolawole@gmail.com>
2017-04-18 11:49:10 -0600

    New db

--- khanatek/settings/production.py
+++ khanatek/settings/production.py
@@ -15,8 +15,8 @@ TEMPLATES[0]['OPTIONS']['debug'] = False
 
 
 # Parse database configuration from $DATABASE_URL
-
-DATABASES =  {'default' : dj_database_url.config()}
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES.['default'].update(db_from_env)
 	
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
